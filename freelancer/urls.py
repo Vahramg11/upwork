a8d56c51a0aa74a06b638949a065c762422d5c03
Vahram <vahramg11@gmail.com>
2024-07-04 21:05:32 +0400

    edit freelancer

--- freelancer/urls.py
+++ freelancer/urls.py
@@ -4,5 +4,6 @@ from .views import FreelancerStaff
 
 urlpatterns = [
     path("freelancers/", FreelancerStaff.as_view({"get": "get_freelancers"})),
-    path("freelancer/<int:pk>/", FreelancerStaff.as_view({"get": "get_one_freelancer"}))
+    path("freelancer/<str:username>/", FreelancerStaff.as_view({"get": "get_one_freelancer"})),
+    path("change_info/freelancer/<str:pk>/", FreelancerStaff.as_view({"patch": "change_freelancer_info"}))
 ]
--- freelancer/views.py
+++ freelancer/views.py
@@ -1,12 +1,12 @@
 from django.db.models import Prefetch, OuterRef
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Freelancer
-from .serializers import FreelancerSerializer, FreelancerDetails
+from .serializers import FreelancerSerializer, FreelancerDetails, ChangeFreelancerInfo
 from job.models import Job
 
 
@@ -22,20 +22,23 @@ class FreelancerStaff(viewsets.ViewSet):
         return Response(data=data_serial.data)
 
     @action(methods=["GET"], detail=True)
-    def get_one_freelancer(self, request, pk):
+    def get_one_freelancer(self, request, username):
         one_freelancer = Freelancer.objects.prefetch_related(
             Prefetch('job', queryset=Job.objects.all()),
-        ).get(id=pk)
+        ).get(username=username)
         data_serial = FreelancerDetails(instance=one_freelancer)
         print(data_serial.data)
         return Response(data_serial.data, status=status.HTTP_200_OK)
 
     @action(methods=["PATCH"], detail=True)
-    def change_freelancer_info(self, reqeust):
+    def change_freelancer_info(self, request, username):
+        freelancer = get_object_or_404(Freelancer, username=username)
+        print(freelancer)
         try:
-            if reqeust.data:
-                pass
-        finally:
-            pass
-
-
+            if request.data:
+                check = ChangeFreelancerInfo(freelancer, data=request.data)
+                if check.is_valid():
+                    new = check.save()
+                    return Response(data=FreelancerDetails(new).data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
